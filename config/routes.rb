6bcf049840018b1b56d3f4e058cc941384071095
jaime-loopstudio <jgarcia@loopstudio.dev>
2023-09-04 13:05:07 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :providers
-  resources :antennas
+  
   namespace :api do
-    namespace :v1 do 
+    namespace :v1 do
+      root 'customers#index'
+
       resources :services
       resources :customers
+      resources :antennas
+      resources :providers
     end
   end
+
+  devise_for :users
+
 end
